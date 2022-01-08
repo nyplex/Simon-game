18c4ef5368d7f64d8ab0885f1fcc1320338098ee
Alex <nypels.alexandre@outlook.com>
2022-01-08 12:28:20 +0000

    Cleaning code

--- src/assets/js/display.js
+++ src/assets/js/display.js
@@ -5,6 +5,7 @@ export let displayMenu = () => {
 
 let displayRules = () => {
     $("#rules-btn").on("click", () => {
+        console.log("hello world");
         $("#setup-modal").slideUp(() => {
             $("#rules-modal").slideDown()
             $("#rules-modal").scrollTop(0)
